00da08641520f68c40edf9b4f7abda82585dc3bc
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-12-10 23:57:07 +0530

    data map

--- application/src/Home/Home.jsx
+++ application/src/Home/Home.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import {useSelector, useDispatch} from "react-redux" 
-import { getData } from '../redux/actions'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getData } from "../redux/actions";
 
 export const Home = () => {
-  const [data, setData]= useState()
-  const productData= useSelector(((state)=> state.getProduct.products))
-  console.log(productData);
-const dispatch= useDispatch(); 
+  const [data, setData] = useState();
+  const { products, isloading, isError } = useSelector(
+    (state) => state.getProduct
+  );
+  console.log(products);
+  const dispatch = useDispatch();
 
-useEffect(()=>{
-  // dispatch(getData)
-  getData(dispatch)
-},[dispatch])
+  useEffect(() => {
+    // dispatch(getData)
+    getData(dispatch);
+  }, [dispatch]);
 
   return (
     <div>
-      {
-        productData.map((ele)=>{
-          
-        })
-      }
+      {products.map((ele) => {
+        return (
+          <div>
+            <img src={ele.Poster} alt="" />
+            <p>Runtime: <span>{ele.Runtime}</span></p>
+            <p>Title: <span>{ele.Title}</span></p>
+            <p>Year: <span>{ele.Year}</span></p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
