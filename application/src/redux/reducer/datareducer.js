e1761ae769665a6ac2af3878165c1fc8579a8eaa
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-12-10 19:22:13 +0530

    add data reducer

--- application/src/redux/reducer/datareducer.js
+++ application/src/redux/reducer/datareducer.js
@@ -1,1 +1,45 @@
-import { GET_DATA, Add_TO_CART, REMOVE_CART } from "../actiontype/actionType";
+import {
+  GET_DATA,
+  Add_TO_CART,
+  REMOVE_CART,
+  ERR_DATA,
+  REQ_DATA,
+} from "../actiontype/actionType";
+
+const initialstate = {
+  data: [],
+  isloading: false,
+  isError: false,
+  filterData: [],
+  products: [],
+};
+
+export const reducer = (state = initialstate, { type, payload }) => {
+  switch (type) {
+    case REQ_DATA:
+      return {
+        ...state,
+        isloading: true,
+        isError: false,
+        filterData: [],
+        products: [],
+      };
+    case ERR_DATA:
+      return {
+        ...state,
+        isloading: false,
+        isError: true,
+        filterData: [],
+        products: [],
+      };
+    case GET_DATA:
+      return {
+        ...state,
+        isloading: false,
+        isError: false,
+        filterData: [],
+        products: [],
+        products: payload,
+      };
+  }
+};
