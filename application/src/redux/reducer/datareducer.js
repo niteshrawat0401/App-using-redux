be52b1b2db6348b882ecc8f8bd26f8a77f030fda
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-12-11 13:56:05 +0530

    prepare product detail reducer

--- application/src/redux/reducer/datareducer.js
+++ application/src/redux/reducer/datareducer.js
@@ -44,3 +44,43 @@ export const getProductreducer = (state = initialstate, { type, payload }) => {
       return state;
   }
 };
+
+
+const initial = {
+  data: [],
+  isloading: false,
+  isError: false,
+  // filterData: [],
+  product: [],
+};
+
+export const getProductdetailreducer = (state = initial, { type, payload }) => {
+  switch (type) {
+    case REQ_DATA:
+      return {
+        ...state,
+        isloading: true,
+        isError: false,
+        // filterData: [],
+        product: [],
+      };
+    case ERR_DATA:
+      return {
+        ...state,
+        isloading: false,
+        isError: true,
+        // filterData: [],
+        product: [],
+      };
+    case GET_DATA:
+      return {
+        ...state,
+        isloading: false,
+        isError: false,
+        // filterData: [],
+        product: payload,
+      };
+    default:
+      return state;
+  }
+};
