beca1c4e7e99f1978ff3876caf032e26f942b3cc
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-12-11 11:33:42 +0530

    css add to data

--- application/src/Home/Home.jsx
+++ application/src/Home/Home.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getData } from "../redux/actions";
+import { getData } from "../redux/actions/dataaction";
+import style from "./Home.module.css"
 
 export const Home = () => {
   const [data, setData] = useState();
@@ -17,15 +18,20 @@ export const Home = () => {
     getData(dispatch);
   }, [dispatch]);
 
+  const addToCart=()=>{
+    
+  }
+
   return (
-    <div>
-      {products.map((ele) => {
+    <div className={style.mainContainer}>
+      {products.map((ele,index) => {
         return (
-          <div>
-            <img src={ele.Poster} alt="" />
-            <p>Runtime: <span>{ele.Runtime}</span></p>
-            <p>Title: <span>{ele.Title}</span></p>
-            <p>Year: <span>{ele.Year}</span></p>
+          <div className={style.innerContainer} key={index}> 
+            <img src={ele.image} alt="" />
+            <p>Runtime: <span>{ele.title}</span></p>
+            <p>Title: <span>{ele.category}</span></p>
+            <p>Year: <span>{ele.price}</span></p>
+            <button onClick={addToCart}>Add to cart</button>
           </div>
         );
       })}
---
+++ application/src/redux/actions/dataaction.js
@@ -0,0 +1,25 @@
+import { Add_TO_CART,  GET_DATA, REMOVE_CART, REQ_DATA, ERR_DATA  } from "../actiontype/actionType";
+import axios from "axios"
+
+export const getData=(dispatch)=>{
+    dispatch(req_data());
+    axios.get(`https://fakestoreapi.com/products`)
+    .then((res)=>
+        dispatch({
+            type: GET_DATA, 
+            payload: res.data
+        })
+        )
+    .catch((err)=>{
+        dispatch(error_data(err.message))
+    })
+}
+
+export const req_data= ()=>({
+    type: REQ_DATA
+})
+
+export const  error_data= (err)=>({
+    type: ERR_DATA,
+    payload: err
+})
